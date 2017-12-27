22e21976ad83dd02174a9a965d2a6c07595aff0e
Jonathan Llewellyn <jllewellyn@atomicfiction.com>
2017-12-27 14:37:08 -0500

    qt: added authors and descritption for v4.8.6

--- qt/4.8.6/package.py
+++ qt/4.8.6/package.py
@@ -3,8 +3,17 @@ name = "qt"
 version = "4.8.6"
 
 authors = [
-    "Troltech"
+    'The Qt Company <https://www.qt.io/>',
+    'Qt Project <https://www.qt.io/>',
 ]
+
+description = \
+    '''
+    Qt is a cross-platform application framework that is used for developing
+    application software that can be run on various software and hardware platforms
+    with little or no change in the underlying codebase, while still being a native
+    application with native capabilities and speed.
+    '''
 
 build_requires = [
     "gcc-4.8.2+"
