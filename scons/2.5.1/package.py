1bd9cf71f4c3e0ddd06865da4cc7b693e2b573af
Jonathan Llewellyn <jllewellyn@atomicfiction.com>
2017-12-27 14:48:46 -0500

    scons: added authors and description

--- scons/2.5.1/package.py
+++ scons/2.5.1/package.py
@@ -3,12 +3,30 @@ name = "scons"
 version = "2.5.1"
 
 authors = [
-    ""
+    'Chad Austin',
+    'Dirk Baechle',
+    'Charles Crain',
+    'William Deegan',
+    'Steve Leblanc',
+    'Rob Managan',
+    'Greg Noel',
+    'Gary Oberbrunner',
+    'Anthony Roach',
+    'Greg Spencer',
+    'Tom Tanner',
+    'Anatoly Techtonik',
+    'Christoph Wiedemann',
+    'Russel Winder'
 ]
 
 description = \
-    """
-    """
+    '''
+    This is SCons, a tool for building software (and other files).  SCons is
+    implemented in Python, and its "configuration files" are actually Python
+    scripts, allowing you to use the full power of a real scripting language
+    to solve build problems.  You do not, however, need to know Python to
+    use SCons effectively.
+    '''
 
 build_requires = [
     "python-2.7"
